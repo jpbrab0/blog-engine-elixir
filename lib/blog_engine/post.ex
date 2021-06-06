c6b361e2946d3704e0f36099a4de7a41a82da963
Joao Pedro <jpresende502@gmail.com>
2021-06-06 19:53:08 -0300

    feat: making schema.

--- lib/blog_engine/post.ex
+++ lib/blog_engine/post.ex
@@ -1,7 +1,25 @@
 defmodule BlogEngine.Post do 
   use Ecto.Schema
 
-  schema "posts" do 
+  import Ecto.Changeset
+
+  @required [:title, :content, :author_name]
+  @optional [:tags]
+
+  schema "posts" do
     field :title, :string
+    field :content, :string
+    field :author_name, :string
+
+    field :tags, :string
+
+    timestamps()
+  end
+
+  def changeset(model \\ %__MODULE__{}, params) do 
+    model
+    |> cast(params, @required ++ @optional)
+    |> validate_required(@required)
+    |> validate_length(:author, min: 3)
   end
 end
