ec3fceb82284d1300e527ea2c06ba41fc597d0d3
Joao Pedro <jpresende502@gmail.com>
2021-06-24 19:06:03 -0300

    feat: Creating a file for run author querys.

--- lib/blog_engine/authors.ex
+++ lib/blog_engine/authors.ex
@@ -1,2 +1,21 @@
 defmodule BlogEngine.Authors do
+  alias BlogEngine.Authors.Schemas.Author
+  alias BlogEngine.Repo
+
+  def get_author(author_id) do
+    try do
+      result = Repo.get_by!(Author, id: author_id)
+
+      {:ok, result}
+    rescue
+      Ecto.NoResultsError ->
+        {:error, "Non found result of this author id."}
+    end
+  end
+
+  def create_author(params) do
+    params
+    |> Author.changeset()
+    |> Repo.insert()
+  end
 end
