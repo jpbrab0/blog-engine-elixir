dad5766334e69124a781dd6dac1d7f06ead2f544
Joao Pedro <jpresende502@gmail.com>
2021-06-23 21:03:25 -0300

    fix: Fixing a bug in authors migration, a line is on wrong place.

--- priv/repo/migrations/20210606224900_create_authors_table.exs
+++ priv/repo/migrations/20210606224900_create_authors_table.exs
@@ -2,6 +2,16 @@ defmodule BlogEngine.Repo.Migrations.CreateAuthorsTable do
   use Ecto.Migration
 
   def change do
+    create table(:authors) do
+      add :name, :string
+      add :email, :string
+      add :nickname, :string
+      add :bio, :string
+      add :age, :integer
 
+      timestamps()
+    end
+    create unique_index(:authors, [:email])
+    create unique_index(:authors, [:nickname])
   end
 end
