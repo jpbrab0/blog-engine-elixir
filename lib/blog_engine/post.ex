fbf85d0c020522bf5ee3563091b8bcad8e9e6a2b
Joao Pedro <jpresende502@gmail.com>
2021-06-23 21:04:26 -0300

    feat: Creating the posts module.

--- lib/blog_engine/post.ex
+++ lib/blog_engine/post.ex
@@ -1,25 +1,2 @@
-defmodule BlogEngine.Post do 
-  use Ecto.Schema
-
-  import Ecto.Changeset
-
-  @required [:title, :content, :author_name]
-  @optional [:tags]
-
-  schema "posts" do
-    field :title, :string
-    field :content, :string
-    field :author_name, :string
-
-    field :tags, :string
-
-    timestamps()
-  end
-
-  def changeset(model \\ %__MODULE__{}, params) do 
-    model
-    |> cast(params, @required ++ @optional)
-    |> validate_required(@required)
-    |> validate_length(:author, min: 3)
-  end
+defmodule BlogEngine.Post do
 end
